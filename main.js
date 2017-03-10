b6a283720dad6188ddcb6bf3a7a54e1da085b2ac
Jesse Waddell <jesse@Jesses-MacBook-Pro.local>
2017-03-10 11:38:30 -0600

    all tables made in db

--- main.js
+++ main.js
@@ -5,4 +5,17 @@ const sqlite3 = require('sqlite3').verbose()
 const DB = new sqlite3.Database('Bangazon.sqlite')
     // console.log(DB)
 
-DB.run("CREATE TABLE IF NOT EXISTS customers (customerName TEXT, streetAddress TEXT, city TEXT, state TEXT, postalCode INT, phoneNumber INT)")
+// DB.run("CREATE TABLE IF NOT EXISTS customers (customerName TEXT, streetAddress TEXT, city TEXT, state TEXT, postalCode INT, phoneNumber INT)")
+
+// DB.run("CREATE TABLE IF NOT EXISTS payment_options (paymentOptId INT, paymentOptName TEXT, paymentOptActNum INT)")
+
+// DB.run("CREATE TABLE IF NOT EXISTS products (productId INT, productName TEXT, productPrice INT)")
+
+// DB.run("CREATE TABLE IF NOT EXISTS orders (orderId INT, customerId TEXT, paymentOptId TEXT, paid TEXT)")
+
+DB.run("CREATE TABLE IF NOT EXISTS order_line_items (itemId INT, orderId INT, productId INT)")
+
+// const popCustomers = () => {
+
+//     const { list } = require()
+// }
